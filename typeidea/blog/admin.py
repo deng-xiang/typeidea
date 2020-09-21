7c1dd90fb11da482f26e4a61406d201e0517409b
deng-xiang <john_dx@163.com>
2020-09-21 18:03:48 +0800

    修改了admin页面

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -5,14 +5,40 @@ from django.utils.html import format_html
 from .models import Post, Category, Tag
 # Register your models here.
 
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """自定义过滤器只只展示当前用户的分类"""
+
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id','name')
+
+    def queryset(self, request, queryset):
+        category_id= self.value()
+        if category_id:
+            return queryset.filter(category_id=category_id)
+        return queryset
+
+
+
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'status', 'is_nav', 'created_time')
+    list_display = ('name', 'status', 'is_nav', 'created_time','post_count')
     fields = ('name','status','is_nav')
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(CategoryAdmin,self).save_model(request,obj,form,change)
+    def post_count(self,obj):
+        return obj.post_set.count()
+    post_count.short_description = '文章数量'
+
+
+
+ #   def __str__(self):
+ #      return self.name
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -25,14 +51,15 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    exclude = ('owner',)
     list_display = [
         'title','category','status',
         'created_time','operator',
     ]
     list_display_links = []
 
-    list_filter = ['category']
-    search_fields = ['title','category_name']
+    list_filter = [CategoryOwnerFilter]
+    search_fields = ['title','category__name']
 
     actions_on_top = True
     actions_on_bottom = True
@@ -40,12 +67,33 @@ class PostAdmin(admin.ModelAdmin):
     #编辑页面
     save_on_top = True
 
-    fields = (
-        ('category','title'),
-        'desc',
-        'status',
-        'content',
-        'tag',
+    #fields = (
+     #   ('category','title'),
+      #  'desc',
+       # 'status',
+     #   'content',
+     #   'tag',
+    #)
+    fieldsets = (
+        ('基础配置', {
+            'description':'基础配置描述',
+            'fields':(
+                ('title','category'),
+                'status',
+            ),
+        }),
+        ('内容', {
+            'fields':(
+                'desc',
+                'content',
+            ),
+        }),
+        ('额外信息',{
+            'classes':('collapse',),
+            'fields':(
+                ('tag',),
+            )
+        })
     )
 
     def operator(self, obj):
@@ -58,3 +106,8 @@ class PostAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.owner=request.user
         return super(PostAdmin,self).save_model(request,obj,form,change)
+
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(owner = request.user)
